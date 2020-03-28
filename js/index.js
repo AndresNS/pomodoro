dc15b13fcba2c574df3389d371f234a014051195
andresns <andresm.navarro@gmail.com>
2020-03-28 19:07:02 -0400

    add timer prototype

--- js/index.js
+++ js/index.js
@@ -1,6 +1,51 @@
 "use strict";
 
-const timer = document.getElementById("timer");
+
+function Timer(initialMinutes, initialSeconds) {
+	this.timerId = 0;
+	this.initialMinutes = initialMinutes;
+	this.initialSeconds = initialSeconds;
+	this.currentMinute = initialMinutes;
+	this.currentSecond = initialSeconds;
+}
+
+//Start Timer
+Timer.prototype.start = function(){
+	this.timerId = setInterval(function(){
+		
+		//Stops timer when done
+		if (this.currentMinute == 0 && this.currentSecond == 0) {
+			clearInterval(this.timerId);
+		}
+
+		if (this.currentSecond == 0) {
+			this.currentMinute -= 1;
+			this.currentSecond = 59;
+		} else {
+			this.currentSecond--;
+		}
+	}, 1000);
+};
+
+//Pause Timer
+Timer.prototype.pause = function(){
+	clearInterval(this.timerId);
+};
+
+//Stop and reset Timer
+Timer.prototype.stop = function(){
+	clearInterval(this.timerId);
+	this.currentMinute = this.initialMinutes;
+	this.currentSecond = this.initialSeconds;
+};
+
+
+
+
+
+
+
+const timeDisplay = document.getElementById("timer");
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const resetButton = document.getElementById("reset");
@@ -10,15 +55,15 @@ const secInput = document.getElementById("sec");
 let intervalID = 0;
 let initialMin = minInput.value;
 let initialSec = secInput.value;
-timer.textContent = `${formatNumber(initialMin)}:${formatNumber(initialSec)}`;
+timeDisplay.textContent = `${formatNumber(initialMin)}:${formatNumber(initialSec)}`;
 
-startButton.addEventListener("click", function(){
+startButton.addEventListener("click", function () {
 	startTimer();
 });
-stopButton.addEventListener("click", function(){
+stopButton.addEventListener("click", function () {
 	stopTimer(intervalID);
 });
-resetButton.addEventListener("click", function(){
+resetButton.addEventListener("click", function () {
 	resetTimer(intervalID);
 });
 
@@ -34,7 +79,7 @@ function startTimer() {
 			clearInterval(intervalID);
 		}
 
-		timer.textContent = `${formatNumber(min)}:${formatNumber(sec)}`;
+		timeDisplay.textContent = `${formatNumber(min)}:${formatNumber(sec)}`;
 
 		if (sec == 0) {
 			min -= 1;
@@ -46,20 +91,16 @@ function startTimer() {
 	}, 1000);
 }
 
-function stopTimer(intervalID){
+function stopTimer(intervalID) {
 	clearInterval(intervalID);
 }
 
-function resetTimer(intervalID){
+function resetTimer(intervalID) {
 	let min = document.getElementById("min").value;
 	let sec = document.getElementById("sec").value;
 	clearInterval(intervalID);
-	timer.textContent = `${formatNumber(min)}:${formatNumber(sec)}`;
+	timeDisplay.textContent = `${formatNumber(min)}:${formatNumber(sec)}`;
 }
-
-
-
-
 
 function formatNumber(num) {
 	return ("0" + num).slice(-2);
