49ae4e244be63291303842fbbf329a1f1205c90f
andresns <andresm.navarro@gmail.com>
2020-04-27 17:19:38 -0400

    change stop button name to pause and fix transition between timers

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ let longBreak = new Timer(LongBreakMinutes, LongBreakSeconds);
 // Get DOM elements
 const timeDisplay = document.getElementById("timer");
 const startButton = document.getElementById("start");
-const stopButton = document.getElementById("stop");
+const pauseButton = document.getElementById("pause");
 const resetButton = document.getElementById("reset");
 const minInput = document.getElementById("min");
 const secInput = document.getElementById("sec");
@@ -34,57 +34,13 @@ startButton.addEventListener("click", function () {
 	pomodoro.start();
 });
 
-stopButton.addEventListener("click", function () {
+pauseButton.addEventListener("click", function () {
 	pomodoro.pause();
 });
 
 resetButton.addEventListener("click", function () {
-	pomodoro.stop();
+	pomodoro.reset();
 });
-
-
-// function startTimer() {
-
-
-// 	let min = document.getElementById("min").value;
-// 	let sec = document.getElementById("sec").value;
-
-// 	intervalID = setInterval(function () {
-
-// 		if (min == 0 && sec == 0) {
-// 			clearInterval(intervalID);
-// 		}
-
-// 		timeDisplay.textContent = `${formatNumber(min)}:${formatNumber(sec)}`;
-
-// 		if (sec == 0) {
-// 			min -= 1;
-// 			sec = 59;
-// 		} else {
-// 			sec--;
-// 		}
-
-// 	}, 1000);
-// }
-
-// function stopTimer(intervalID) {
-// 	clearInterval(intervalID);
-// }
-
-// function resetTimer(intervalID) {
-// 	let min = document.getElementById("min").value;
-// 	let sec = document.getElementById("sec").value;
-// 	clearInterval(intervalID);
-// 	timeDisplay.textContent = `${formatNumber(min)}:${formatNumber(sec)}`;
-// }
-
-function formatNumber(num) {
-	return ("0" + num).slice(-2);
-}
-
-
-
-
 
 
 
@@ -103,29 +59,26 @@ function Timer(initialMinutes, initialSeconds) {
 }
 
 //Start Timer
-// Timer.prototype.start = function () {
-// 	this.running = true;
-// 	return new Promise((resolve, reject) => {
-// 		this.timerId = setInterval(() => {
-// 			timeDisplay.textContent = `${formatNumber(this.currentMinute)}:${formatNumber(this.currentSecond)}`;
+Timer.prototype.start = function (iterator) {
+	this.running = true;
+	this.timerId = setInterval(() => {
+		timeDisplay.textContent = `${formatNumber(this.currentMinute)}:${formatNumber(this.currentSecond)}`;
+		console.log(`${formatNumber(this.currentMinute)}:${formatNumber(this.currentSecond)}`);
 
-// 			//Stops timer when done
-// 			if (this.currentMinute == 0 && this.currentSecond == 0) {
-// 				clearInterval(this.timerId);
-// 				this.done = true;
-// 				this.running = false;
-// 				resolve("done");
-// 			}
+		//Stops timer when done
+		if (this.currentMinute == 0 && this.currentSecond == 0) {
+			this.reset();
+			iterator.next();
+		}
 
-// 			if (this.currentSecond == 0) {
-// 				this.currentMinute -= 1;
-// 				this.currentSecond = 59;
-// 			} else {
-// 				this.currentSecond--;
-// 			}
-// 		}, 1000);
-// 	});
-// };
+		if (this.currentSecond == 0) {
+			this.currentMinute -= 1;
+			this.currentSecond = 59;
+		} else {
+			this.currentSecond--;
+		}
+	}, 1000);
+};
 
 //Pause Timer
 Timer.prototype.pause = function () {
@@ -133,8 +86,8 @@ Timer.prototype.pause = function () {
 	this.running = false;
 };
 
-//Stop and reset Timer
-Timer.prototype.stop = function () {
+//Reset Timer
+Timer.prototype.reset = function () {
 	clearInterval(this.timerId);
 	this.running = false;
 	this.done = true;
@@ -149,40 +102,9 @@ Timer.prototype.stop = function () {
 
 
 
-
-
-Timer.prototype.start = function (iterator) {
-	this.running = true;
-	this.timerId = setInterval(() => {
-		timeDisplay.textContent = `${formatNumber(this.currentMinute)}:${formatNumber(this.currentSecond)}`;
-		console.log(`${formatNumber(this.currentMinute)}:${formatNumber(this.currentSecond)}`);
-
-		//Stops timer when done
-		if (this.currentMinute == 0 && this.currentSecond == 0) {
-			clearInterval(this.timerId);
-			this.done = true;
-			this.running = false;
-			iterator.next();
-		}
-
-		if (this.currentSecond == 0) {
-			this.currentMinute -= 1;
-			this.currentSecond = 59;
-		} else {
-			this.currentSecond--;
-		}
-	}, 1000);
-};
-
-
-
 let it = runSequence();
 //starts the sequence
 it.next();
-
-
-// console.log(test());
-
 
 function* runSequence() {
 	for (let i = 0; i < sequence.length; i++) {
@@ -200,13 +122,8 @@ function* runSequence() {
 	}
 }
 
-// function test() {
-// 	return new Promise((resolve, reject) => {
 
-// 		setInterval(() => {
-// 			resolve("done");
-// 		}, 1000);
-// 	});
-// }
-
-// console.log(test());
+//Helper functions
+function formatNumber(num) {
+	return ("0" + num).slice(-2);
+}
