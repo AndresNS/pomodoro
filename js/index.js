ee2940dd7eb8f4d8c3c6e544421d573258c5f30f
andresns <andresm.navarro@gmail.com>
2020-04-27 13:13:44 -0400

    add transition between timers with iterator

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 "use strict";
+/*eslint indent: ["error", "tab", { "SwitchCase": 1 }]*/
 
 //Default values
 let pomodoroMinutes = 0;
-let pomodoroSeconds = 5;
-let shortBreakMinutes = 5;
-let shortBreakSeconds = 0;
-let LongBreakMinutes = 10;
-let LongBreakSeconds = 0;
+let pomodoroSeconds = 3;
+let shortBreakMinutes = 0;
+let shortBreakSeconds = 5;
+let LongBreakMinutes = 0;
+let LongBreakSeconds = 10;
 
 // 0 = pomodoro, 1 = short break, 2 = long break
 let sequence = [0, 1, 0, 1, 0, 1, 0, 2];
@@ -15,35 +16,32 @@ let pomodoro = new Timer(pomodoroMinutes, pomodoroSeconds);
 let shortBreak = new Timer(shortBreakMinutes, shortBreakSeconds);
 let longBreak = new Timer(LongBreakMinutes, LongBreakSeconds);
 
-
-
-
-
+// Get DOM elements
 const timeDisplay = document.getElementById("timer");
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const resetButton = document.getElementById("reset");
-
 const minInput = document.getElementById("min");
 const secInput = document.getElementById("sec");
+
+//Set initial value for inputs
 let initialMin = minInput.value;
 let initialSec = secInput.value;
-
-// let intervalID = 0;
 timeDisplay.textContent = `${formatNumber(initialMin)}:${formatNumber(initialSec)}`;
 
+//Button listeners
 startButton.addEventListener("click", function () {
-	// startTimer();
 	pomodoro.start();
 });
+
 stopButton.addEventListener("click", function () {
-	// stopTimer(intervalID);
 	pomodoro.pause();
 });
+
 resetButton.addEventListener("click", function () {
-	// resetTimer(intervalID);
 	pomodoro.stop();
 });
+
 
 // function startTimer() {
 
@@ -105,30 +103,29 @@ function Timer(initialMinutes, initialSeconds) {
 }
 
 //Start Timer
-Timer.prototype.start = function () {
-	this.running = true;
-	return new Promise((resolve, reject) => {
-		this.timerId = setInterval(() => {
-			timeDisplay.textContent = `${formatNumber(this.currentMinute)}:${formatNumber(this.currentSecond)}`;
-			//Stops timer when done
-			if (this.currentMinute == 0 && this.currentSecond == 0) {
-				clearInterval(this.timerId);
-				this.done = true;
-				this.running = false;
-				console.log("pre promise resolve");
-				resolve("done");
-				console.log("post promise resolve");
-			}
-	
-			if (this.currentSecond == 0) {
-				this.currentMinute -= 1;
-				this.currentSecond = 59;
-			} else {
-				this.currentSecond--;
-			}
-		}, 1000);
-	});
-};
+// Timer.prototype.start = function () {
+// 	this.running = true;
+// 	return new Promise((resolve, reject) => {
+// 		this.timerId = setInterval(() => {
+// 			timeDisplay.textContent = `${formatNumber(this.currentMinute)}:${formatNumber(this.currentSecond)}`;
+
+// 			//Stops timer when done
+// 			if (this.currentMinute == 0 && this.currentSecond == 0) {
+// 				clearInterval(this.timerId);
+// 				this.done = true;
+// 				this.running = false;
+// 				resolve("done");
+// 			}
+
+// 			if (this.currentSecond == 0) {
+// 				this.currentMinute -= 1;
+// 				this.currentSecond = 59;
+// 			} else {
+// 				this.currentSecond--;
+// 			}
+// 		}, 1000);
+// 	});
+// };
 
 //Pause Timer
 Timer.prototype.pause = function () {
@@ -154,8 +151,28 @@ Timer.prototype.stop = function () {
 
 
 
+Timer.prototype.start = function (iterator) {
+	this.running = true;
+	this.timerId = setInterval(() => {
+		timeDisplay.textContent = `${formatNumber(this.currentMinute)}:${formatNumber(this.currentSecond)}`;
+		console.log(`${formatNumber(this.currentMinute)}:${formatNumber(this.currentSecond)}`);
 
+		//Stops timer when done
+		if (this.currentMinute == 0 && this.currentSecond == 0) {
+			clearInterval(this.timerId);
+			this.done = true;
+			this.running = false;
+			iterator.next();
+		}
 
+		if (this.currentSecond == 0) {
+			this.currentMinute -= 1;
+			this.currentSecond = 59;
+		} else {
+			this.currentSecond--;
+		}
+	}, 1000);
+};
 
 
 
@@ -163,21 +180,33 @@ let it = runSequence();
 //starts the sequence
 it.next();
 
+
+// console.log(test());
+
+
 function* runSequence() {
 	for (let i = 0; i < sequence.length; i++) {
 		switch (sequence[i]) {
 			case 0:
-				yield pomodoro.start();
+				yield pomodoro.start(it);
 				break;
 			case 1:
-				yield shortBreak.start();
+				yield shortBreak.start(it);
 				break;
 			case 2:
-				yield longBreak.start();
+				yield longBreak.start(it);
 				break;
 		}
 	}
 }
 
+// function test() {
+// 	return new Promise((resolve, reject) => {
 
+// 		setInterval(() => {
+// 			resolve("done");
+// 		}, 1000);
+// 	});
+// }
 
+// console.log(test());
