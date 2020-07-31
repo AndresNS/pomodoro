bef8f652d2d77f30f80e9494182ac55453a1bb42
andresns <andresm.navarro@gmail.com>
2020-07-31 14:26:14 -0400

    add header  animations.

--- js/nav.js
+++ js/nav.js
@@ -1,39 +1,38 @@
 "use strict";
 
-const hamburger = document.querySelector(".header__hamburger");
-const headerNav = document.querySelector(".header__nav");
-const btnCloseNav = document.querySelector(".header__close");
-const headerLogo = document.querySelector(".header__logo");
 const header = document.querySelector("header");
-// const menuItems = document.querySelectorAll(".menu li");
+const headerNav = document.querySelector(".header__nav");
+const headerLogo = document.querySelector(".header__logo");
+const hamburger = document.querySelector(".header__hamburger");
+const btnCloseNav = document.querySelector(".header__close");
+const btnCloseNavImg = document.querySelector(".header__close>a>img");
+const menuItems = document.querySelectorAll(".menu li");
 
-let w = header.offsetWidth;
-let logow = headerLogo.getBoundingClientRect().width;
-let aux = w/2 - logow/2;
+let headerWidth = header.offsetWidth;
+let logoWidth = headerLogo.getBoundingClientRect().width;
+let translateValue = Math.round(headerWidth / 2 - logoWidth / 2);
+
 window.onresize = function () {
-	w = header.offsetWidth;
-	logow = headerLogo.getBoundingClientRect().width;
-	aux = w/2 - logow/2;
+	headerWidth = header.offsetWidth;
+	logoWidth = headerLogo.getBoundingClientRect().width;
+	translateValue = Math.round(headerWidth / 2 - logoWidth / 2 - 40);
 };
 
-
 hamburger.addEventListener("click", () => {
-	headerNav.classList.toggle("open");
-	btnCloseNav.classList.toggle("visible");
-	console.log(aux);
-	headerLogo.style.transform = "translateX(" + aux + "px)";
-	hamburger.style.display = "none";
-	// menuItems.forEach(item => {
-	// 	item.classList.toggle("fade");
-	// });
+	toggleMenu(translateValue);
 });
 
 btnCloseNav.addEventListener("click", () => {
+	toggleMenu(0);
+});
+
+function toggleMenu(translateValue){
 	headerNav.classList.toggle("open");
 	btnCloseNav.classList.toggle("visible");
-	headerLogo.style.transform = "translateX(0px)";
-	hamburger.style.display = "block";
-	// menuItems.forEach(item => {
-	// 	item.classList.toggle("fade");
-	// });
-});
+	headerLogo.style.transform = "translateX(" + translateValue + "px)";
+	hamburger.classList.toggle("hidden");
+	btnCloseNavImg.classList.toggle("close");
+	menuItems.forEach(item => {
+		item.classList.toggle("fade");
+	});
+}
