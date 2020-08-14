b7e0dfd31d7600e9c4f3301d7a5d6762c2262607
andresns <andresm.navarro@gmail.com>
2020-08-14 11:18:41 -0400

    add click event on sequence display controls

--- js/timer-sequence-display.js
+++ js/timer-sequence-display.js
@@ -1,2 +1,21 @@
-const sequenceList = document.querySelectorAll(".sequence-display__list>li");
-console.log(sequenceList);
+const sequenceDisplayList = document.querySelector(".sequence-display__list");
+const sequenceDisplayControls = document.querySelectorAll(".sequence-display__controls>a");
+const currentListItem = sequenceDisplayList.firstElementChild;
+let listItemWidth = currentListItem.offsetWidth;
+let listItemTranslateValue = 0;
+let currentIndex = 1;
+let maxIndex = sequenceDisplayList.childElementCount - 1;
+
+sequenceDisplayControls.forEach((item) => {
+	item.addEventListener("click", () => {
+		if (item.classList.contains("left") && currentIndex > 1) {
+			listItemTranslateValue = listItemTranslateValue + listItemWidth;
+			sequenceDisplayList.style.transform = "translateX(" + listItemTranslateValue + "px)";
+			currentIndex -= 1;
+		} else if (item.classList.contains("right") && currentIndex < maxIndex) {
+			listItemTranslateValue = listItemTranslateValue + (- listItemWidth);
+			sequenceDisplayList.style.transform = "translateX(" + listItemTranslateValue + "px)";
+			currentIndex += 1;
+		}
+	});
+});
