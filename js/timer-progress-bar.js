d755fc7a474a829198e2c8022f61ddb6743c5d17
andresns <andresm.navarro@gmail.com>
2020-08-21 12:48:36 -0400

    change desktop progress bar style and add animation to it

--- js/timer-progress-bar.js
+++ js/timer-progress-bar.js
@@ -1,111 +1,51 @@
-/*eslint indent: ["error", "tab", { "SwitchCase": 1 }]*/
-
 /* PROGRESS BAR DESKTOP */
 
 const sequence = {
 	cicles: [0, 1, 0, 1, 0, 1, 0, 2],
-	pomMin: 25,
-	shortBreakMins: 5,
-	longBreakMins: 10
+	pomMin: .1,
+	shortBreakMins: .05,
+	longBreakMins: .08
 };
 
-const widths = calculateWidths(sequence.pomMin, sequence.shortBreakMins, sequence.longBreakMins);
 
 const progressBarCurrent = document.querySelector(".timer__progress-bar__current");
-const progressBarBg = document.querySelector(".timer__progress-bar__bg");
-
-
-sequence.cicles.map((i) => {
-	if (i === 0) {
-		addElement("div", "pomodoro", 0, progressBarCurrent);
-		addElement("div", "pomodoro", widths.pomodoroWidth, progressBarBg);
-	} else if (i === 1) {
-		addElement("div", "break", 0, progressBarCurrent);
-		addElement("div", "break", widths.shortBreakWidth, progressBarBg);
-	} else {
-		addElement("div", "break", 0, progressBarCurrent);
-		addElement("div", "break", widths.longBreakWidth, progressBarBg);
-	}
-});
-
-const intervalBlocks = document.querySelectorAll(".timer__progress-bar__current div");
+let barWidth = 0;
 let idx = 0;
-let currentTimer = intervalBlocks[idx];
-let w = 0;
-currentTimer.style.width = w + "%";
-let finalWidth = widths.pomodoroWidth;
-
-let t = setInterval(() => {
-	currentTimer.style.width = w + "%";
-	w++;
-	if (w >= finalWidth) {
+progressBarCurrent.style.width = barWidth + "%";
+progressBarCurrent.classList.add("pomodoro");
+let timer = setInterval(() => {
+	if (barWidth >= 100) {
 		nextInterval();
 	}
-}, 100);
 
-function nextInterval() {
-	if (idx >= sequence.cicles.length-1) {
-		clearInterval(t);
-	} else {
-		idx++;
-		if (sequence.cicles[idx] == 0) {
-			finalWidth = widths.pomodoroWidth;
-		} else if (sequence.cicles[idx] == 1) {
-			finalWidth = widths.shortBreakWidth;
-		} else if (sequence.cicles[idx] == 2) {
-			finalWidth = widths.longBreakWidth;
-		}
-		w = 0;
-		currentTimer = intervalBlocks[idx];
+	if (sequence.cicles[idx] == 0) {
+		barWidth += (100 / (sequence.pomMin * 60));
+	} else if (sequence.cicles[idx] == 1) {
+		barWidth += (100 / (sequence.shortBreakMins * 60));
+	} else if (sequence.cicles[idx] == 2) {
+		barWidth += (100 / (sequence.longBreakMins * 60));
 	}
 
+	progressBarCurrent.style.width = barWidth + "%";
+}, 1000);
 
-}
+// FIX CLEAR INTERVAL ON LAST INTERVAL
 
-// let it = runSequence();
-// //starts the sequence
-// // it.next();
-
-// function* runSequence() {
-// 	sequence.cicles.map((i)=>{
-// 		switch (sequence.cicles[i]) {
-// 			case 0:
-// 				yield pomodoro.start(it);
-// 				break;
-// 			case 1:
-// 				yield shortBreak.start(it);
-// 				break;
-// 			case 2:
-// 				yield longBreak.start(it);
-// 				break;
-// 		}
-// 	});
-// }
-
-
-// Helper functions
-
-function calculateWidths(pomMins, shortBreakMins, longBreakMins) {
-	const pomodoroInSeconds = pomMins * 60;
-	const shortBreakInSeconds = shortBreakMins * 60;
-	const longBreakInSeconds = longBreakMins * 60;
-
-	const totalSeconds = pomodoroInSeconds + shortBreakInSeconds + longBreakInSeconds;
-
-	const pomodoroWidth = pomodoroInSeconds * 100 / totalSeconds;
-	const shortBreakWidth = shortBreakInSeconds * 100 / totalSeconds;
-	const longBreakWidth = longBreakInSeconds * 100 / totalSeconds;
-
-	return {
-		pomodoroWidth,
-		shortBreakWidth,
-		longBreakWidth
-	};
-}
-
-function addElement(tag, className, width, parent) {
-	const element = document.createElement("div");
-	element.className = className;
-	element.style.width = width + "%";
-	parent.appendChild(element);
+function nextInterval() {
+	idx += 1;
+	if (idx < sequence.cicles.length) {
+		barWidth = 0;
+		// click arrow
+		const nextIntervalArrow = document.querySelectorAll(".sequence-display__controls>a");
+		nextIntervalArrow[1].click();
+		if (sequence.cicles[idx] == 0) {
+			progressBarCurrent.classList.remove("break");
+			progressBarCurrent.classList.add("pomodoro");
+		} else {
+			progressBarCurrent.classList.remove("pomodoro");
+			progressBarCurrent.classList.add("break");
+		}
+	} else {
+		clearInterval(timer);
+	}
 }
