6ce37da1024da7be555b3d38e1719d6bd448fe95
andresns <andresm.navarro@gmail.com>
2020-09-09 13:13:43 -0400

    add modal for block type selection

--- js/index.js
+++ js/index.js
@@ -248,3 +248,43 @@ function getDragAfterElement(container, y) {
 }
 
 /******* END SETTINGS HELPER FUNCTIONS *******/
+
+/******* MODAL *******/
+
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const modalOverlay = document.getElementById("overlay");
+
+openModalButtons.forEach((button)=>{
+	button.addEventListener("click", ()=>{
+		const modal = document.querySelector(button.dataset.modalTarget);
+		openModal(modal);
+	});
+});
+
+closeModalButtons.forEach((button)=>{
+	button.addEventListener("click", ()=>{
+		const modal = button.closest(".modal");
+		closeModal(modal);
+	});
+});
+
+modalOverlay.addEventListener("click", ()=>{
+	const modals = document.querySelectorAll(".modal.active");
+	modals.forEach((modal)=>{
+		closeModal(modal);
+	});
+});
+
+function openModal(modal){
+	if(modal == null) return;
+	modal.classList.add("active");
+	modalOverlay.classList.add("active");
+}
+
+function closeModal(modal){
+	if(modal == null) return;
+	modal.classList.remove("active");
+	modalOverlay.classList.remove("active");
+}
+/******* END MODAL *******/
